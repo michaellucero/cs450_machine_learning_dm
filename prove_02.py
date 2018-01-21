104607ba58cee0fe285064ecef3a02027c284426
Michael Lucero <michaellucero@users.noreply.github.com>
2018-01-20 22:46:37 -0700

    Finish implementation of my own K nearest Neighbor algorithm

--- prove_02.py
+++ prove_02.py
@@ -14,14 +14,16 @@ class HardCodedModel:
 	"""
 	Shell for future model
 	"""
-	def __init__(self):
+	def __init__(self, ):
 		pass
 	
+	"K-NN"
 	def predict(self, testing_data):
 
 		predicted_targets = []
 		
 		for test in testing_data:
+			text[0] = test[0] - 
 			predicted_targets.append(0)
 		return predicted_targets
 
@@ -29,8 +31,8 @@ class HardCodedClassifier:
 	"""
 	Shell for future classifier
 	"""	
-	def __init__(self):
-		pass
+	def __init__(self, k_neighbors):
+		self.k_neighbors = k_neighbors
 		
 	def fit(self, training_data, training_targets):
 		self.hard_coded_model = HardCodedModel()
@@ -40,40 +42,40 @@ class HardCodedClassifier:
 
 iris = datasets.load_iris()
 
-# # Show the data (the attributes of each instance)
-# print("Iris Data:")
-# print(iris.data)
-# print()
+# Show the data (the attributes of each instance)
+print("Iris Data:")
+print(iris.data)
+print()
 
-# # Show the target values (in numeric format) of each instance
-# print("Iris Target:")
-# print(iris.target)
-# print()
+# Show the target values (in numeric format) of each instance
+print("Iris Target:")
+print(iris.target)
+print()
 
-# # Show the actual target names that correspond to each number
-# print("Iris Target Names")
-# print(iris.target_names)
-# print()
+# Show the actual target names that correspond to each number
+print("Iris Target Names")
+print(iris.target_names)
+print()
 
-# # combines all attributes together into one array for verifying
-# data_target = []
-# for data, target in zip(iris.data, iris.target):
-# 	data_target.append((data, target, iris.target_names[target]))	
+# combines all attributes together into one array for verifying
+data_target = []
+for data, target in zip(iris.data, iris.target):
+	data_target.append((data, target, iris.target_names[target]))	
 
-# #print(data_target)
-# print()
+#print(data_target)
+print()
 
 
-# # randomize data into 70% training set and 30% testing set
-# data_train, data_test, target_train, target_test = train_test_split(iris.data, iris.target, train_size=0.70, test_size=0.30)
+# randomize data into 70% training set and 30% testing set
+data_train, data_test, target_train, target_test = train_test_split(iris.data, iris.target, train_size=0.70, test_size=0.30)
 
-# print("70% of original")
-# print("Training Data:")
-# print(data_train)
-# print()
-# print("Training Target:")
-# print(target_train)
-# print()
+print("70% of original")
+print("Training Data:")
+print(data_train)
+print()
+print("Training Target:")
+print(target_train)
+print()
 
 
 # # create model
@@ -101,13 +103,13 @@ iris = datasets.load_iris()
 # print("Accuracy In Prediction: {:.2f}".format(accuracy))
 
 
-# """
-# My Classifier
-# """
-# print("My Hard Coded Classifer which will always return 0")
-# classifier = HardCodedClassifier()
-# model = classifier.fit(data_train, target_train)
-# targets_predicted = model.predict(data_test)
+"""
+My Classifier
+"""
+print("My Hard Coded Classifer which will always return 0")
+classifier = HardCodedClassifier()
+model = classifier.fit(data_train, target_train)
+targets_predicted = model.predict(data_test)
 
 # print("30% of original")
 # print("Testing Data:")
